f84be05f2282c4d68bddeb2cc40e2b215d971d9d
DJMF2015 <djm.fulton@gmail.com>
2019-05-18 15:48:19 +0100

    add version code with added extensions for lizard and spock. Dom handlers and UI also included. fixes to make to reset to zero method and to refactoring to-do

---
+++ RPSL/README.md
@@ -0,0 +1,15 @@
+<h2 > simple browser-based 'rock, paper, scissors, Lizard, Spock' game (javascript reworking)</h2>
+
+<b> As based on the exercise at The Odin Project: https://www.theodinproject.com/courses/javascript
+</b>
+
+
+
+
+
+
+
+
+
+
+27/08/2018
---
+++ RPSL/js/application.js
@@ -0,0 +1,114 @@
+const game = document.querySelectorAll(".move-box input");
+const roundText = document.querySelector("#round");
+const playerSelectionText = document.querySelector("#player-selection");
+const computerSelectionText = document.querySelector("#computer-selection");
+const resultsText = document.querySelector("#results-container");
+const playerScoreText = document.querySelector("#human-player .score-box");
+const computerScoreText = document.querySelector("#pc-player .score-box");
+const tiesText = document.querySelector("#ties-text .ties-score");
+const finalscore = document.querySelector("#final-score");
+
+let round = 0;
+let ties = 0;
+let computerPoints = 0;
+let playerPoints = 0;
+
+game.forEach( move => move.addEventListener("click",event => {
+  let playerSelection = event.target.name;
+  let computerSelection = computerPlay();
+  playRound(playerSelection,computerSelection);
+})
+);
+
+const computerPlay = function(){
+  let hand = {
+    0: "paper",
+    1 : "rock",
+    2 : "scissors",
+    3 : "lizard",
+    4 : "spock"
+  };
+
+  const randomIndex = Math.floor(Math.random() * 4);
+  console.log(hand[randomIndex])
+  return hand[randomIndex];
+}
+
+const playRound = function(playerSelection, computerSelection) {
+  let result;
+
+  round++;
+  roundText.textContent = `round: ${round}`;
+
+  playerSelectionText.textContent = `you selected ${playerSelection}`;
+
+  if (playerSelection === computerSelection ){
+    result = "tie";
+    ties++;
+  }  else if (playerSelection === "scissors" && computerSelection === "paper"){
+    result = "win"
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    result = "win"
+  } else if (playerSelection === "rock" && computerSelection === "lizard") {
+    result = "win"
+  } else if (playerSelection === "lizard" && computerSelection === "spock") {
+    result = "win"
+  } else if (playerSelection === "spock" && computerSelection === "scissors") {
+    result = "win"
+  } else if (playerSelection === "scissors" && computerSelection === "lizard") {
+    result = "win"
+  } else if (playerSelection === "lizard" && computerSelection === "paper") {
+    result = "win"
+  } else if (playerSelection === "paper" && computerSelection === "spock"){
+    result = "win"
+  } else if (playerSelection === "spock" && computerSelection === "rock") {
+    result = "win"
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    result = "win"
+
+  } else  {
+    result = "lose";
+  }
+
+  if(result === "tie"){
+    resultsText.textContent = `it's a tie!`;
+    console.log(ties)
+  }
+
+  if(result === "win"){
+    resultsText.textContent = `you win!, ${playerSelection} beats ${computerSelection}`;
+    playerPoints++;
+    playerScoreText.textContent = `${playerPoints} `;
+  }
+
+  if(result === "lose"){
+    resultsText.textContent = `you lose!, ${computerSelection} beats ${playerSelection}`;
+    computerPoints++;
+    computerScoreText.textContent = `${computerPoints} `;
+  }
+  if(playerPoints == 5){
+    finalscore.textContent = `Total Rounds: ${round}, Ties: ${ties} YOU WIN. Congratulations!`
+    resetScore();
+  }
+  if(computerPoints == 5){
+    finalscore.textContent = `Total Rounds: ${round}, Ties: ${ties} SORRY - YOU LOSE. Hard Luck`;
+    resetScore();
+  }
+
+  function resetScore(){
+    round = 0;
+    ties = 0;
+    computerPoints = 0;
+    playerPoints = 0;
+    // roundText.textContent = `round: ${round}`;
+    // playerScoreText.textContent = `${playerPoints}`;
+    computerScoreText.textContent = `${computerPoints}`;
+    resultsText.textContent = ``;
+    playerSelectionText.textContent = ``;
+    computerSelectionText.textContent = ``;
+
+  }
+
+
+
+}
