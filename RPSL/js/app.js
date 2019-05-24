1209dcea571c8c69d41f3f8956293e945deabea4
DJMF2015 <djm.fulton@gmail.com>
2019-05-24 20:02:49 +0100

    add change to styles

--- RPSL/js/app.js
+++ RPSL/js/app.js
@@ -5,8 +5,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let playerSelection = event.target.name;
 
-    // event.target.style.background = 'linear-gradient(white,#012d)';
-    // setTimeout(()=>{ event.target.style.background ="";},1000);
     let computerSelection = computerPlay();
     playRound(playerSelection,computerSelection);
   }));
--- RPSL/js/application.js
+++ RPSL/js/application.js
@@ -8,7 +8,6 @@ const computerScoreText = document.querySelector("#pc-player .score-box");
 const tiesText = document.querySelector("#ties-text .ties-score");
 const finalscore = document.querySelector("#final-score");
 const gamestats = document.querySelector("#gameStats-container")
-// const section = document.querySelector("#player ")
 
 let round = 0;
 let ties = 0;
@@ -24,7 +23,7 @@ const computerPlay = function(){
     4 : "spock"
   };
 
-  const randomIndex = Math.floor(Math.random() * 4);
+  const randomIndex = Math.floor(Math.random() * 5);
   console.log(hand[randomIndex])
   return hand[randomIndex];
 }
@@ -33,66 +32,72 @@ const playRound = function(playerSelection, computerSelection) {
   let result;
   round++;
   roundText.textContent = `Round: ${round}`;
+
   playerSelectionText.textContent = `You Selected ${playerSelection}`;
 
-  if (playerSelection === computerSelection ){
+  if (this.playerSelection === computerSelection ){
     result = "tie";
     ties++;
-  }  else if (playerSelection === "scissors" && computerSelection === "paper"){
-    result = "win"
-  } else if (playerSelection === "paper" && computerSelection === "rock") {
-    result = "win"
-  } else if (playerSelection === "rock" && computerSelection === "lizard") {
-    result = "win"
-  } else if (playerSelection === "lizard" && computerSelection === "spock") {
-    result = "win"
-  } else if (playerSelection === "spock" && computerSelection === "scissors") {
-    result = "win"
-  } else if (playerSelection === "scissors" && computerSelection === "lizard") {
-    result = "win"
-  } else if (playerSelection === "lizard" && computerSelection === "paper") {
-    result = "win"
-  } else if (playerSelection === "paper" && computerSelection === "spock"){
-    result = "win"
-  } else if (playerSelection === "spock" && computerSelection === "rock") {
-    result = "win"
-  } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    result = "win"
-  } else  {
-    result = "lose";
   }
+ else if (playerSelection === "scissors" && computerSelection === "paper"){
+  result = "win"
+} else if (playerSelection === "paper" && computerSelection === "rock") {
+  result = "win"
+} else if (playerSelection === "rock" && computerSelection === "lizard") {
+  result = "win"
+} else if (playerSelection === "lizard" && computerSelection === "spock") {
+  result = "win"
+} else if (playerSelection === "spock" && computerSelection === "scissors") {
+  result = "win"
+} else if (playerSelection === "scissors" && computerSelection === "lizard") {
+  result = "win"
+} else if (playerSelection === "lizard" && computerSelection === "paper") {
+  result = "win"
+} else if (playerSelection === "paper" && computerSelection === "spock"){
+  result = "win"
+} else if (playerSelection === "spock" && computerSelection === "rock") {
+  result = "win"
+} else if (playerSelection === "rock" && computerSelection === "scissors") {
+  result = "win"
+} else  {
+  result = "lose";
+}
 
-  if(result === "tie"){
-    resultsText.textContent = `it's a tie!`;
-    console.log(ties)
-  }
+if(result === "tie"){
+  resultsText.textContent = `it's a tie!`;
+  // h1.textContent = `TIE!!`;
+  console.log(ties)
+}
 
-  if(result === "win"){
-    resultsText.style.color="black ";
-    gamestats.style.background="linear-gradient(white, #016c)";
-    setTimeout(function(){gamestats.style.background ="";},1000);
-    resultsText.textContent = `YOU WIN!, ${playerSelection} BEATS ${computerSelection}`;
-    playerPoints++;
-    playerScoreText.textContent = `${playerPoints} `;
-  }
+if(result === "win"){
+  resultsText.style.color="red ";
+  resultsText.style.opacity = 1;
+  resultsText.textContent = `WIN!`;
+  gamestats.style.background="linear-gradient(white, #016c)";
+  setTimeout(function(){gamestats.style.background ="";},1000);
+  resultsText.textContent = `YOU WIN!, ${playerSelection} BEATS ${computerSelection}`;
+  playerPoints++;
+  playerScoreText.textContent = `${playerPoints} `;
+}
 
-  if(result === "lose"){
-    resultsText.style.color="black";
-    gamestats.style.background="linear-gradient(red, #016c)";
-    setTimeout(function(){gamestats.style.background ="";},1000);
-    resultsText.textContent = `YOU LOSE!, ${computerSelection} BEATS ${playerSelection}`;
-    computerPoints++;
-    computerScoreText.textContent = `${computerPoints} `;
-  }
-  if(playerPoints == 5){
-    finalscore.textContent = `Total Rounds: ${round} Ties: ${ties} YOU WIN.`
-    // alert(`Total Rounds: ${round}, Ties: ${ties} YOU WIN. Congratulations!`);
-    resetScore();
-  }
-  if(computerPoints == 5){
+if(result === "lose"){
+  resultsText.style.color="black";
+  resultsText.style.opacity = -1;
+  resultsText.textContent = `LOSE!`;
+  gamestats.style.background="linear-gradient(red, #016c)";
+  setTimeout(function(){gamestats.style.background ="";},1000);
+  resultsText.textContent = `YOU LOSE!, ${computerSelection} BEATS ${playerSelection}`;
+  computerPoints++;
+  computerScoreText.textContent = `${computerPoints} `;
+}
+if(playerPoints == 5){
+  finalscore.textContent = `Total Rounds: ${round} Ties: ${ties} YOU WIN.`
+  resetScore();
+}
+if(computerPoints == 5){
   finalscore.textContent = `Total Rounds: ${round}, Ties: ${ties} SORRY - YOU LOSE. Hard Luck`;
-    resetScore();
-  }
+  resetScore();
+}
 }
 
 function resetScore(){
